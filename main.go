7d67569a00d87b25754341b148e3a2b49101a2e0
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-09-29 00:26:29 -0400

    adds method to unmarshal yaml

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"gopkg.in/yaml.v3"
+)
+
+var mock = `
+bool: true
+str_quoted: "this is a test"
+str_unquoted: this is a test
+list:
+- 1
+- 2
+- 3
+nested:
+  foo: bar
+`
 
 func main() {
-	fmt.Println("cast files from castings and structured data")
+	data := unmarshalInputs(mock)
+	fmt.Printf("--- m:\n%v\n\n", data)
+}
+
+func unmarshalInputs(inputs string) map[interface{}]interface{} {
+	data := make(map[interface{}]interface{})
+	err := yaml.Unmarshal([]byte(inputs), &data)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	return data
 }
