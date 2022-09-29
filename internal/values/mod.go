c52f17be54154f47ee8aea0e396276d3f52715b9
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-09-29 10:18:04 -0400

    adds parse from file to values module

--- internal/values/mod.go
+++ internal/values/mod.go
@@ -1,14 +1,25 @@
 package values
 
 import (
+	"os"
+
 	goYaml "gopkg.in/yaml.v3"
 )
 
 type Values map[interface{}]interface{}
 
-// Unmarshals values into a generic map data structure.
+// Unmarshals yaml string into a generic map data structure.
 func Parse(yaml string) (Values, error) {
 	values := make(Values)
 	err := goYaml.Unmarshal([]byte(yaml), &values)
 	return values, err
+}
+
+// Unmarshals yaml file into a generic map data structure.
+func ParseFile(valuesFilePath string) (Values, error) {
+	valBytes, err := os.ReadFile(valuesFilePath)
+	if err != nil {
+		return nil, err
+	}
+	return Parse(string(valBytes))
 }
