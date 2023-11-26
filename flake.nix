41f0df7b0e10891aba2959162005cd3551316128
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-11-26 18:22:18 -0500

    build project with nix (using gomod2nix)

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Cakemix nix configurations";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/23.05";
+    utils.url = "github:numtide/flake-utils";
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "utils";
+    };
+  };
+
+  outputs = { utils, nixpkgs, gomod2nix, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ gomod2nix.overlays.default ];
+        };
+        cakemix = pkgs.buildGoApplication {
+          pname = "cakemix";
+          version = "1.0.0";
+          src = ./.;
+          modules = ./gomod2nix.toml;
+        };
+      in
+      with pkgs; {
+        packages = {
+          default = cakemix;
+          cakemix = cakemix;
+        };
+        devShell = mkShell {
+          buildInputs = [
+            go
+            gopls
+            gotools
+            go-tools
+            gomod2nix.packages.${system}.default
+            just
+          ];
+          shellHook = ''
+            # Keep Go cache and module files in the project
+            export GOPATH="$(pwd)/.go"
+          '';
+        };
+      });
+}
--- shell.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = [ pkgs.buildPackages.go_1_18 pkgs.buildPackages.gopls pkgs.buildPackages.just ];
-}
-
