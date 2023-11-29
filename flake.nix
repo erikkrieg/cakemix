71bcd7168dda2070856e6df2323cfb930b4b5f0c
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-11-29 00:40:37 -0500

    increment version in flake

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
         };
         cakemix = pkgs.buildGoApplication {
           pname = "cakemix";
-          version = "1.0.0";
+          version = "1.0.1";
           src = ./.;
           modules = ./gomod2nix.toml;
         };
