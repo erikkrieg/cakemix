45f3afbb071d46109f695a9c034d9dd8f71b5128
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-11-28 23:42:46 -0500

    basic implementation for prompting unset values

--- internal/values/mod.go
+++ internal/values/mod.go
@@ -1,6 +1,7 @@
 package values
 
 import (
+	"fmt"
 	"os"
 
 	goYaml "gopkg.in/yaml.v3"
@@ -8,14 +9,24 @@ import (
 
 type Values map[interface{}]interface{}
 
-// Unmarshals yaml string into a generic map data structure.
+// Parse unmarshals yaml string into a generic map data structure.
+// For nil values the user is prompted to provide a value (can be left empty).
 func Parse(yaml string) (Values, error) {
 	values := make(Values)
 	err := goYaml.Unmarshal([]byte(yaml), &values)
+	for key, val := range values {
+		if val == nil {
+			var tmp string
+			fmt.Printf("Provide value for [%s]: ", key)
+			fmt.Scanf("%s", &tmp)
+			values[key] = tmp
+		}
+	}
 	return values, err
 }
 
-// Unmarshals yaml file into a generic map data structure.
+// ParseFile unmarshals yaml file into a generic map data structure.
+// For nil values the user is prompted to provide a value (can be left empty).
 func ParseFile(valuesFilePath string) (Values, error) {
 	valBytes, err := os.ReadFile(valuesFilePath)
 	if err != nil {
